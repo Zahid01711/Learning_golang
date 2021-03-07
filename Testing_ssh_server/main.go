9425eb322058495ba22c4dbc874bf6da6077c0d8
MD Zahidul Islam <54137658+jahid01711@users.noreply.github.com>
2021-03-07 23:05:42 +0600

    creating ssh server using go and make sure to move the "github.com" to src folder
    make sure to move the "github.com" to src folder

---
+++ Testing_ssh_server/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"io"
+	"log"
+
+	"github.com/gliderlabs/ssh"
+)
+
+func main() {
+	ssh.Handle(func(s ssh.Session) {
+		io.WriteString(s, "Hello world\n")
+	})
+
+	log.Fatal(ssh.ListenAndServe(":2222", nil))
+}
